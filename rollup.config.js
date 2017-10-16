789ac63ad4ff6d85b8280cd4927a69dd6c78c2b1
vasco <vastur@gmail.com>
2017-10-15 18:07:08 -0700

    Migrate to rollup for improved tree-shaking

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonJs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+
+export default {
+    input: 'src/index.js',
+    output: [
+        {
+            format: 'umd',
+            name: 'ColorLegend',
+            file: 'dist/d3-color-legend.js',
+            sourcemap: true
+        },
+        {
+            format: 'es',
+            file: 'dist/d3-color-legend.mjs'
+        }
+    ],
+    plugins: [
+        nodeResolve(),
+        commonJs(),
+        babel({
+            presets: [
+                ["es2015", { "modules": false }]
+            ],
+            plugins: ["external-helpers"],
+            babelrc: false
+        })
+    ]
+};
